0dccc7bae38000d7c00956fc4e5a26d04cbdbc1d
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2022-12-18 11:15:04 +0530

    Added html

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ var database_connections_status = "";
 connection.connect((err) => {
   if (err) {
     console.log("Error connecting to Db");
-    database_connections_status = "<h1>Error connecting to Db</h1>";
+    database_connections_status = "<h3>Error connecting to Db</h3>";
     return;
   }
   console.log("Connection established");
-  database_connections_status = "<h1>Connection established</h1>";
+  database_connections_status = "<h3>Connection established</h3>";
 });
 
 app.use(bodyParser.json());
@@ -33,7 +33,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   // res.sendFile(__dirname + "/login.html");
-  res.send(database_connections_status);
+  res.send(
+    `
+  <body>
+    <h1>Registration</h1>
+    ` +
+      database_connections_status +
+      `
+    <form action="/server.js" method="post">
+        <input type="text" placeholder="username" name="user">
+        <input type="text" placeholder="name" name="name">
+        <input type="password" placeholder="password" name="pswd">
+        <input type="submit" value="submit">
+    </form>
+</body>`
+  );
 });
 
 app.post("/server.js", (req, res) => {
